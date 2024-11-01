cef5673bf7c85b8d91adb95badba3bb326b621b6
Rajasah2000 <rajasah30030@gmail.com>
2024-11-01 13:33:25 +0530

    up

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-const cors = require("cors"); // Import cors
+const cors = require("cors");
 
 const adminRoutes = require("./routes/adminRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
+
 dotenv.config();
 
 const app = express();
@@ -21,13 +22,23 @@ app.use(
   })
 );
 
+// Handle preflight requests
+app.options("*", cors());
+
+// Middleware to parse JSON requests
 app.use(express.json());
 
-//All Routes
+// All Routes
 app.use("/api/admin", adminRoutes);
 app.use("/api/admin/category", categoryRoutes);
 app.use("/api/admin/products", productRoutes);
 app.use("/api", userRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 // Start server
 const PORT = process.env.PORT || 8000;
